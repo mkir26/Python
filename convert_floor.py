81fcf8f5ba1dc4f87360735ff3f316b43a2910ed
KIR <108181715+mkir26@users.noreply.github.com>
2022-07-07 00:47:08 +0300

    third week

---
+++ convert_floor.py
@@ -0,0 +1,5 @@
+# Convert number of floor (USA to EUR)
+print('This is a converter!')
+sinp = input('Please, input a number of floor (EUR):')
+sinp = int(sinp) + 1
+print('And the floor number in USA is:', sinp)
---
+++ task31.py
@@ -0,0 +1,8 @@
+hrs = input('Enter Hours:')
+h = float(hrs)
+rate = input('Enter Rate per hours:')
+r = float(rate)
+if h <= 40:
+    print(h * r)
+else:
+    print((h - 40) * r * 1.5 + 40 * r)
